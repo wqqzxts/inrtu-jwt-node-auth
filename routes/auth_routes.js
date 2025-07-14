22710fd766c5de31819d4da40244089dbce0b856
Timur Semennikov <wqqzxts@gmail.com>
2025-07-14 21:05:03 +0800

    made auth service boilerplate & fix issues in routes

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
-const authMiddleware = require('../middleware/auth');
 
 router.post('/api/v1/auth/register', authController.register);
 router.post('/api/v1/auth/login', authController.login);
 router.post('/api/v1/auth/logout', authController.logout);
-router.post('/api/v1/auth/register', authController.refresh);
-// router.post('/api/v1/auth/logout');
+router.post('/api/v1/auth/refresh', authController.refresh);
 
 module.exports = router;
---
+++ services/auth.js
@@ -0,0 +1,23 @@
+const db = require('../debug/db');
+const jwtService = require('../services/jwt');
+const config = require('../config');
+
+class AuthService {
+    async register(userData) {
+
+    }
+
+    async login(email, password) {
+
+    }
+
+    async logout() {
+
+    }
+
+    async refresh() {
+
+    }
+}
+
+module.exports = new AuthService();
