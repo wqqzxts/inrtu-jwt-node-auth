4a21c2eff8b2dc9746b2f80e449700d687b626c6
Timur Semennikov <wqqzxts@gmail.com>
2025-07-14 16:40:13 +0800

    made generation and verification methods for jwt tokens

---
+++ services/jwt.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+class JwtService {
+    genAccess(userId) {
+        return jwt.sign({ sub: userId }, config.jwt.secret, { expiresIn: config.jwt.expAccess });
+    }
+
+    verifyAccess(token) {
+        return jwt.verify(token, config.jwt.secret);
+    }
+
+    genRefresh(userId) {
+        return jwt.sign({ sub: userId }, config.jwt.secret, { expiresIn: config.jwt.expRefresh });
+    }
+
+    verifyRefresh(token) {
+        return jwt.verify(token, config.jwt.secret);
+    }
+}
+
+module.exports = new JwtService();
